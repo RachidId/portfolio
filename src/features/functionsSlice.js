a7ae94192a1d45b82f219847bd2a16673038ef5d
Macbook dore <macbookdore@MacBook-de-Macbook.local>
2023-10-08 07:33:42 +0100

    change editProd to true

--- src/features/functionsSlice.js
+++ src/features/functionsSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     addingProd: false,
-    editingProd: false
+    editingProd: true
 };
 
 export const functionsSlice = createSlice({
